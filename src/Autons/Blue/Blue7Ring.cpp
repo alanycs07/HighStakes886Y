c99f4e64d1375b3553822e6884b4b1afb4f4165c
Zhou <mzhou@southridge.bc.ca>
2025-03-01 04:57:33 -0800

    idk whats wrong with imu im going to bed

--- src/Autons/Blue/Blue7Ring.cpp
+++ src/Autons/Blue/Blue7Ring.cpp
@@ -11,23 +11,68 @@ void Blue7Ring(){
     chassis.setPose(61.5, 11.6, 163.3);
     armTarget = wallPos;
     pros::delay(600);
-    chassis.turnToPoint(29, 25, 1000, {.forwards = false, .minSpeed = 20, .earlyExitRange = 7});
+    chassis.turnToPoint(25, 24, 1000, {.forwards = false, .minSpeed = 20, .earlyExitRange = 7});
     chassis.waitUntilDone();
     armTarget = restingPos;
-    chassis.moveToPoint(29, 25, 1500, {.forwards = false, .maxSpeed = 80});
+    chassis.moveToPoint(25, 24, 1500, {.forwards = false, .maxSpeed = 90});
     chassis.waitUntilDone();
     goalClamp.set_value(true);
     pros::delay(100);  
-    chassis.turnToPoint(14.5, 40, 800);
+    chassis.turnToPoint(11, 41, 800);
     chassis.waitUntilDone();
-    runAntiJam = true;
-    sortingColor = true;
-    chassis.moveToPoint(14.5, 40, 800, {.minSpeed = 30, .earlyExitRange = 6});
-    chassis.turnToPoint(9, 47, 400, {.minSpeed = 30, .earlyExitRange = 10});
-    chassis.moveToPoint(9, 47, 400, {.minSpeed = 20, .earlyExitRange = 10});
+    // runAntiJam = true;
+    // sortingColor = true;
 
-    // chassis.swingToPoint(23, 47, DriveSide::RIGHT, 800);
-    // chassis.moveToPoint(23, 47, 1000);
+    intake.move_velocity(12000);
+
+    // chassis.moveToPoint(14.5, 40, 800, {.minSpeed = 30, .earlyExitRange = 6});
+    // chassis.turnToPoint(9, 47, 400, {.minSpeed = 30, .earlyExitRange = 3});
+    // chassis.moveToPoint(9, 47, 400, {.minSpeed = 20, .earlyExitRange = 3});
+
+    chassis.moveToPoint(11, 41, 800);
+    chassis.turnToPoint(8, 50, 800);
+    chassis.moveToPoint(8, 50, 800);
+
+    chassis.swingToPoint(26, 44, DriveSide::RIGHT, 800);
+    // chassis.moveToPoint(26, 44, 1000);
+    // chassis.moveToPoint(23, 47, 600, {.forwards = false});
+
+
+    // chassis.turnToPoint(70, 68, 800);
+    // chassis.moveToPoint(70, 68, 2000, {.maxSpeed = 80});
+
+    chassis.turnToPoint(62, 65, 800);
+    chassis.moveToPoint(62, 65, 2000, {.maxSpeed = 80});
+    chassis.moveToPoint(52, 62, 600, {.forwards = false});
+    chassis.waitUntilDone();
+    intakeRaise.set_value(true);
+    intake.move_velocity(0);
+    pros::delay(200);
+    sortingColor = true;
+
+    chassis.moveToPoint(62, 65, 1000);
+    chassis.waitUntilDone();
+    intakeRaise.set_value(false);
+    pros::delay(200);
+    chassis.moveToPoint(52, 62, 500, {.forwards = false});
+    // chassis.moveToPoint(70, 68, 1000, {.maxSpeed = 50});
+    // chassis.waitUntilDone();
+    // intakeRaise.set_value(false);
+    // pros::delay(200);
+    // chassis.moveToPoint(52, 62, 800, {.forwards = false});
+    chassis.turnToPoint(48, 11.5, 800);
+    chassis.moveToPoint(48, 11.5, 1000, {.minSpeed = 30, .earlyExitRange = 5});
+    chassis.moveToPoint(46, 11.5, 1000, {.maxSpeed = 50});
+    doinker.set_value(true);
+    leftDoinker.set_value(true);
+    chassis.waitUntilDone();
+    doinker.set_value(false);
+    // leftDoinker.set_value(false);
+    // chassis.turnToPoint(55.5, -43.5, 300);
+    // chassis.waitUntilDone();
+    // sortingColor = false;
+    // chassis.moveToPoint(55.5, -43.5, 800);
+    // chassis.turnToPoint(70, -70, 800, {.forwards = false});
 
 
     // chassis.setPose(0,0,0);
